1361c52326e154b6f5c25b870f31643bb9337835
adios <adiadita343@gmail.com>
2026-05-14 20:34:49 +0300

    Update ModeSetting.java

--- src/main/java/net/overlord/client/setting/impl/ModeSetting.java
+++ src/main/java/net/overlord/client/setting/impl/ModeSetting.java
@@ -1,26 +1,32 @@
-package net.overlord.client.setting.impl;
+package com.adiadita343;
 
-import net.overlord.client.setting.Setting;
 import java.util.Arrays;
 import java.util.List;
 
-public class ModeSetting extends Setting<String> {
-    private final List<String> modes;
+public class ModeSetting extends Setting {
     private int index;
+    private List<String> modes;
 
-    public ModeSetting(String name, String description, String defaultValue, String... modes) {
-        super(name, description, defaultValue);
+    public ModeSetting(String name, String defaultMode, Module parentMod, String... modes) {
+        super(name, parentMod);
         this.modes = Arrays.asList(modes);
-        this.index = this.modes.indexOf(defaultValue);
+        this.index = this.modes.indexOf(defaultMode);
+    }
+
+    public String getMode() {
+        return modes.get(index);
+    }
+
+    public void cycle() {
+        // Trece la următoarea opțiune din listă, iar după ultima revine la prima
+        if (index < modes.size() - 1) {
+            index++;
+        } else {
+            index = 0;
+        }
     }
 
     public List<String> getModes() {
         return modes;
-    }
-
-    public void cycle() {
-        index++;
-        if (index >= modes.size()) index = 0;
-        this.setValue(modes.get(index));
     }
 }
