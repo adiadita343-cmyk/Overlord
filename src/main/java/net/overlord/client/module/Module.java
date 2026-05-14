7a8456fe64a02530dc9f985cddf3de513b92ad89
adios <adiadita343@gmail.com>
2026-05-14 20:47:52 +0300

    Update Module.java

--- src/main/java/net/overlord/client/module/Module.java
+++ src/main/java/net/overlord/client/module/Module.java
@@ -1,20 +1,18 @@
 package com.adiadita343;
 
-import net.minecraft.client.MinecraftClient;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Module {
     public String name;
     public String category;
-    public boolean enabled = false;
-    // Această listă va ține toate setările (Boolean, Number, Mode)
+    public boolean enabled;
     public List<Setting> settings = new ArrayList<>();
-    protected static MinecraftClient mc = MinecraftClient.getInstance();
 
     public Module(String name, String category) {
         this.name = name;
         this.category = category;
+        this.enabled = false;
     }
 
     public void addSetting(Setting setting) {
@@ -25,7 +23,5 @@ public class Module {
         this.enabled = !this.enabled;
     }
 
-    // Aici va veni logica de hack (ex: killaura, fly)
-    public void onTick() {
-    }
+    public void onTick() {}
 }
