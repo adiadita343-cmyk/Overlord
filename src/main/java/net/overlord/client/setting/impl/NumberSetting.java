67821ec6026842316d233ebf93bd46e32bdee444
adios <adiadita343@gmail.com>
2026-05-14 20:34:18 +0300

    Update NumberSetting.java

--- src/main/java/net/overlord/client/setting/impl/NumberSetting.java
+++ src/main/java/net/overlord/client/setting/impl/NumberSetting.java
@@ -1,18 +1,27 @@
-package net.overlord.client.setting.impl;
+package com.adiadita343;
 
-import net.overlord.client.setting.Setting;
+public class NumberSetting extends Setting {
+    private double value;
+    private double min, max, increment;
 
-public class NumberSetting extends Setting<Double> {
-    private final double min, max, increment;
-
-    public NumberSetting(String name, String description, double defaultValue, double min, double max, double increment) {
-        super(name, description, defaultValue);
+    public NumberSetting(String name, double defaultValue, double min, double max, double increment, Module parentMod) {
+        super(name, parentMod);
+        this.value = defaultValue;
         this.min = min;
         this.max = max;
         this.increment = increment;
     }
 
+    public double getValue() {
+        return value;
+    }
+
+    public void setValue(double value) {
+        // Ne asigurăm că valoarea rămâne între minim și maxim
+        double precision = 1.0 / increment;
+        this.value = Math.round(Math.max(min, Math.min(max, value)) * precision) / precision;
+    }
+
     public double getMin() { return min; }
     public double getMax() { return max; }
-    public double getIncrement() { return increment; }
-};
+}
