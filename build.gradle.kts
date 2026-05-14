97e8a6ec36f852710c346f3fe5a48948e3393bfb
adios <adiadita343@gmail.com>
2026-05-14 18:16:50 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,19 +1,13 @@
 plugins {
     id("fabric-loom") version "1.7-SNAPSHOT"
     id("maven-publish")
-    kotlin("jvm") version "1.9.22"
 }
 
 version = "1.0.0"
 group = "net.overlord"
 
-base {
-    archivesName.set("overlord")
-}
-
 repositories {
     mavenCentral()
-    maven { url = uri("https://maven.fabricmc.net/") }
 }
 
 dependencies {
@@ -21,10 +15,6 @@ dependencies {
     mappings("net.fabricmc:yarn:1.21.1+build.3:v2")
     modImplementation("net.fabricmc:fabric-loader:0.15.11")
     modImplementation("net.fabricmc.fabric-api:fabric-api:0.102.0+1.21.1")
-}
-
-tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
-    kotlinOptions.jvmTarget = "21"
 }
 
 tasks.processResources {
