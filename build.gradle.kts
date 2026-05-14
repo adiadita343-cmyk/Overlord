ea4366f07b324f9682de2c0b34c4fab4c6f5f29d
adios <adiadita343@gmail.com>
2026-05-14 18:14:44 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
-    kotlin("jvm") version "2.0.0"
     id("fabric-loom") version "1.7-SNAPSHOT"
     id("maven-publish")
+    kotlin("jvm") version "1.9.22"
 }
 
 version = "1.0.0"
@@ -12,21 +12,19 @@ base {
 }
 
 repositories {
-    // Locul de unde Gradle descarcă librăriile necesare
     mavenCentral()
+    maven { url = uri("https://maven.fabricmc.net/") }
 }
 
 dependencies {
-    // Versiunile necesare pentru Minecraft 1.21.1
     minecraft("com.mojang:minecraft:1.21.1")
     mappings("net.fabricmc:yarn:1.21.1+build.3:v2")
     modImplementation("net.fabricmc:fabric-loader:0.15.11")
-
-    // Fabric API - esențial pentru majoritatea modurilor
     modImplementation("net.fabricmc.fabric-api:fabric-api:0.102.0+1.21.1")
-    
-    // Kotlin pentru Fabric
-    modImplementation("net.fabricmc:fabric-language-kotlin:1.11.0+kotlin.2.0.0")
+}
+
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
+    kotlinOptions.jvmTarget = "21"
 }
 
 tasks.processResources {
@@ -36,18 +34,7 @@ tasks.processResources {
     }
 }
 
-tasks.withType<JavaCompile>().configureEach {
-    options.release.set(21)
-}
-
 java {
-    withSourcesJar()
     sourceCompatibility = JavaVersion.VERSION_21
     targetCompatibility = JavaVersion.VERSION_21
-}
-
-tasks.jar {
-    from("LICENSE") {
-        rename { "${it}_${project.base.archivesName.get()}" }
-    }
 }
