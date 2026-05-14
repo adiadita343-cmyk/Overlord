dcd69e33fbf66261aecfec3e35dc3cf46e811537
adios <adiadita343@gmail.com>
2026-05-14 17:37:06 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
-    id("fabric-loom") version "1.7-SNAPSHOT"
+    id("fabric-loom") version "1.8-SNAPSHOT"
+    id("maven-publish")
 }
 
 version = "1.0.0"
@@ -7,18 +8,45 @@ group = "net.overlord"
 
 repositories {
     maven { url = uri("https://maven.fabricmc.net/") }
+    mavenCentral()
 }
 
 dependencies {
+    // Versiunea de Minecraft
     minecraft("com.mojang:minecraft:1.21.1")
+    
+    // Mappings (Yarn)
     mappings("net.fabricmc:yarn:1.21.1+build.3:v2")
+    
+    // Fabric Loader
     modImplementation("net.fabricmc:fabric-loader:0.15.11")
+
+    // Fabric API (Opțional, dar recomandat pentru majoritatea modurilor)
+    // modImplementation("net.fabricmc.fabric-api:fabric-api:0.102.0+1.21.1")
 }
 
 tasks.processResources {
     inputs.property("version", project.version)
     filteringCharset = "UTF-8"
+
     filesMatching("fabric.mod.json") {
         expand("version" to project.version)
+    }
+}
+
+tasks.withType<JavaCompile>().configureEach {
+    options.release.set(21)
+    options.encoding = "UTF-8"
+}
+
+java {
+    withSourcesJar()
+    sourceCompatibility = JavaVersion.VERSION_21
+    targetCompatibility = JavaVersion.VERSION_21
+}
+
+tasks.jar {
+    from("LICENSE") {
+        rename { "${it}_${project.name}" }
     }
 }
