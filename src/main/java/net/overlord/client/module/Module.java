8719e069701fd2050db1547d2b926fcb2761372c
adios <adiadita343@gmail.com>
2026-05-15 20:11:22 +0300

    Update Module.java

--- src/main/java/net/overlord/client/module/Module.java
+++ src/main/java/net/overlord/client/module/Module.java
@@ -7,27 +7,26 @@ public class Module {
     public String name;
     public String category;
     public boolean enabled;
-    // Lista de setări (pentru BooleanSetting, NumberSetting etc.)
+    public int keyCode = 0; // ACEASTA ERA LINIA LIPSA!
     public List<Setting> settings = new ArrayList<>();
 
-    // Constructorul principal pe care îl folosește ModuleManager
     public Module(String name, String category) {
         this.name = name;
         this.category = category;
-        this.enabled = false; // Toate modulele pornesc dezactivate
+        this.enabled = false;
     }
 
-    // Metodă pentru a adăuga setări ulterior
     public void addSetting(Setting setting) {
         this.settings.add(setting);
     }
 
-    // Metodă pentru activare/dezactivare
     public void toggle() {
         this.enabled = !this.enabled;
+        if (this.enabled) onEnable();
+        else onDisable();
     }
 
-    // Aici se va scrie logica pentru fiecare hack (se apelează în fiecare frame)
-    public void onTick() {
-    }
+    public void onEnable() {}
+    public void onDisable() {}
+    public void onTick() {}
 }
