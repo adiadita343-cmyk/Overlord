b7af002030395f2d126ea3e27e883340e0b4233b
adios <adiadita343@gmail.com>
2026-05-15 14:38:00 +0300

    Update ModuleManager.java

--- src/main/java/net/overlord/client/module/ModuleManager.java
+++ src/main/java/net/overlord/client/module/ModuleManager.java
@@ -9,12 +9,13 @@ public class ModuleManager {
 
     public static void init() {
         modules.clear();
+        // Categoriile tale de hack-uri
         String[] cats = {"COMBAT", "MOVEMENT", "VISUAL", "PLAYER", "MISC"};
+        
         for (String c : cats) {
-            for (int i = 1; i <= 240; i++) {
-                Module m = new Module(c + "_" + i, c);
-                m.addSetting(new BooleanSetting("Active", true, m));
-                modules.add(m);
+            for (int i = 1; i <= 250; i++) {
+                // Generăm module cu nume unic: COMBAT_1, COMBAT_2... până la 250
+                modules.add(new Module(c + "_" + i, c));
             }
         }
     }
