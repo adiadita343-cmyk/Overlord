5784c1649856790e70d76b9468b4e2d172367dba
adios <adiadita343@gmail.com>
2026-05-15 14:41:03 +0300

    Update ModuleManager.java

--- src/main/java/net/overlord/client/module/ModuleManager.java
+++ src/main/java/net/overlord/client/module/ModuleManager.java
@@ -5,21 +5,27 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class ModuleManager {
+    // Lista principală unde stocăm toate cele 1250 de module
     public static List<Module> modules = new ArrayList<>();
 
     public static void init() {
         modules.clear();
-        // Categoriile tale de hack-uri
-        String[] cats = {"COMBAT", "MOVEMENT", "VISUAL", "PLAYER", "MISC"};
         
-        for (String c : cats) {
+        // Definim categoriile standard pentru un client de hack-uri
+        String[] categories = {"COMBAT", "MOVEMENT", "VISUAL", "PLAYER", "MISC"};
+        
+        for (String cat : categories) {
             for (int i = 1; i <= 250; i++) {
-                // Generăm module cu nume unic: COMBAT_1, COMBAT_2... până la 250
-                modules.add(new Module(c + "_" + i, c));
+                // Generăm module cu nume unic, ex: COMBAT_1, COMBAT_2...
+                // Folosim constructorul simplu (Nume, Categorie)
+                modules.add(new Module(cat + "_" + i, cat));
             }
         }
+        
+        System.out.println("Overlord Client: S-au incarcat " + modules.size() + " module.");
     }
 
+    // Funcție care ne ajută să filtrăm modulele pentru ClickGUI
     public static List<Module> getModulesByCategory(String category) {
         return modules.stream()
                 .filter(m -> m.category.equalsIgnoreCase(category))
