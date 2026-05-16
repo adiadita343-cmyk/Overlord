1bd8f59279ccc14b8990900dc71c4f4a65649b7c
adios <adiadita343@gmail.com>
2026-05-16 12:27:02 +0300

    Update Module.java

--- src/main/java/net/overlord/client/module/Module.java
+++ src/main/java/net/overlord/client/module/Module.java
@@ -1,15 +1,15 @@
-package net.overlord.adiadita343.module;
+package net.overlord.com.adiadita343.module;
 
 import net.minecraft.client.MinecraftClient;
 
 public abstract class Module {
+    // Instanța supremă de Minecraft accesibilă de peste tot
+    public static final MinecraftClient mc = MinecraftClient.getInstance();
+
     public String name;
     public String category;
     public boolean enabled;
-    public int keyCode;
-
-    // Asta lipsea si repara toate erorile din registre!
-    protected static final MinecraftClient mc = MinecraftClient.getInstance();
+    public int keyCode; // Păstrat special pentru ClickGUI-ul tău mov
 
     public Module(String name, String category) {
         this.name = name;
@@ -17,13 +17,16 @@ public abstract class Module {
         this.enabled = false;
     }
 
+    public void onEnable() {}
+    public void onDisable() {}
+    
+    // Metodele noi de evenimente (Sistemul Meteor-Style)
+    public void onTick() {}
+    public void onRender() {}
+
     public void toggle() {
         this.enabled = !this.enabled;
         if (enabled) onEnable();
         else onDisable();
     }
-
-    public void onEnable() {}
-    public void onDisable() {}
-    public abstract void onTick();
 }
